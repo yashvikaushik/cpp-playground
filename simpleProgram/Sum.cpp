32f011fa8a53147a4e838d7d7f4080174580d3db
yashvikaushik <yashvikaushik2525@gmail.com>
2025-07-17 00:27:58 +0530

    sum of two numers

---
+++ simpleProgram/Sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //taking user input
+    int x,y,sum=0;
+    cout<<"enter the two numbers that must be added";
+    cin>> x;
+    cin>> y;
+    sum=x+y;
+    cout<<"the sum is: "<<sum;
+    return 0;
+}
